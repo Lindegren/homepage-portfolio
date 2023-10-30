5e8ef32db5c32768ebd60e26e1a68ff500854476
Lindegren <anton@plateimpact.com>
2023-10-30 13:04:21 +0100

    Created react components, starting to set up Redux

--- src/App.js
+++ src/App.js
@@ -1,55 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import sunIcon from './images/sun.png';
+
+function TaskInputBar() {
+  return (
+    <form className='TaskInputForm'>
+      <input className='TaskInputField' type="text" placeholder="Input your next task" />
+    </form>
+  )
+};
+
+function TaskCard({ task }) {
+  return (
+    <div className="TaskCard">
+      <p> {task.text} </p>
+      <button>Remove task</button>
+      <button>Mark completed</button>
+    </div>
+  )
+};
+
+function TaskTable({ tasks }){
+  return (
+    <div className='TaskTable'>
+      {tasks.map((task) => <TaskCard task={task} />)}
+    </div>
+  )
+};
+
+function PictureContainer({src}) {
+  return (
+    <div className='PictureContainer'>
+      <img src={src} width="300px" />
+      <button>Prev image</button>
+      <button>Next image</button>
+    </div>
+  )
+}
+
+function WeatherContainer({icon, weatherAPI}){
+  return (
+    <div className='WeatherContainer'>
+      <img src={icon} width="40px"/>
+      <p>{weatherAPI} </p>
+    </div>
+  )
+}
+
+function QuoteContainer({quoteAPI}) {
+  return(
+    <div className='QuoteContainer'>
+      <p>{quoteAPI.text}</p>
+    </div>
+  )
+}
+
+const TASKS = [
+  {text: "Feed sourdoug", isCompleted: false},
+  {text: "Get well", isCompleted: false},
+  {text: "Complete portfolio project", isCompleted: false},
+  {text: "Clean the oven", isCompleted: false},
+  {text: "Go to the gym", isCompleted: false}, 
+];
+
+const IMG = [
+  {src: "https://images.unsplash.com/photo-1698375962447-8aac6091e31b?auto=format&fit=crop&q=80&w=1665&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+]
+
+const WEATHERICONS = [{
+  sunny: sunIcon,
+}]
+
+const QUOTE = {
+  text: "This is a quote",
+  src: ""
+};
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <h1>Test header</h1>
+        <TaskInputBar />
+        <TaskTable tasks={TASKS} />
+        <PictureContainer src={IMG[0].src} />
+        <WeatherContainer icon={WEATHERICONS[0].sunny} weatherAPI={"It's sunny today"}/>
+        <QuoteContainer quoteAPI={QUOTE} />
       </header>
     </div>
   );
